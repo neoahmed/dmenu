be9901163d803656daf24a33955d5ae86f87428d
neoahmed <neoahmed@proton.me>
2025-08-16 05:48:03 +0300

    my build with fuzzymatch patch

--- config.def.h
+++ config.def.h
@@ -2,16 +2,18 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy  = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching */
+
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"Liberation Mono:size=13"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+		      /*     fg         bg       */
+	[SchemeNorm] = { "#d4be98", "#282828" },
+	[SchemeSel]  = { "#282828", "#928374" },
+	[SchemeOut]  = { "#282828", "#7daea3" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
