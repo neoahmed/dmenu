93a86d68862064f3af3aa33c13bfc33ce4edf545
neoahmed <neoahmed@proton.me>
2025-09-29 22:21:18 +0300

    chore(scheme): redesign

--- config.def.h
+++ config.def.h
@@ -9,11 +9,18 @@ static const char *fonts[] = {
 	"Liberation Mono:weight=bold:size=13"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
+static const char col_crust[]   = "#11111b";  // dmenu base (bg)
+static const char col_overlay1[]= "#7f849c";  // active panel
+
+static const char col_subtext0[]= "#a6adc8";  // unselected text
+static const char col_surface1[]= "#45475a";  // Surface1 
+
 static const char *colors[SchemeLast][2] = {
 		      /*     fg         bg       */
-	[SchemeNorm] = { "#d4be98", "#282828" },
-	[SchemeSel]  = { "#282828", "#7c6f64" },
-	[SchemeOut]  = { "#282828", "#7daea3" },
+	[SchemeNorm] = { col_subtext0, col_crust    },
+	[SchemeSel]  = { col_crust,    col_overlay1 },
+	[SchemeOut]  = { col_crust,    col_surface1 },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
